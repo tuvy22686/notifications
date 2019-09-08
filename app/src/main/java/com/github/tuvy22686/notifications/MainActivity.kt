4c5f4e9324863b5339417570d5e1553040691b35
Tomosugi Tasaka <tuvy.ano@gmail.com>
2019-09-08 15:49:53 +0900

    enable databinding

--- app/src/main/java/com/github/tuvy22686/notifications/MainActivity.kt
+++ app/src/main/java/com/github/tuvy22686/notifications/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.github.tuvy22686.notifications
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.github.tuvy22686.notifications.databinding.ActivityMainBinding
+import com.github.tuvy22686.notifications.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
+    }
+
+    private val mainViewModel = MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.viewModel = mainViewModel
     }
 }
---
+++ app/src/main/java/com/github/tuvy22686/notifications/viewmodel/MainViewModel.kt
@@ -0,0 +1,5 @@
+package com.github.tuvy22686.notifications.viewmodel
+
+class MainViewModel {
+    var helloWorld = "Hello World from ViewModel"
+}
