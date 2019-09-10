349ea0cc0ad8d5c6773a4070175d8893c50b7c6a
Tomosugi Tasaka <ttasaka@yahoo-corp.jp>
2019-09-10 15:55:53 +0900

    Push通知の実装

--- app/src/main/kotlin/com/github/tuvy22686/notifications/activity/main/MainActivity.kt
+++ app/src/main/kotlin/com/github/tuvy22686/notifications/activity/main/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.github.tuvy22686.notifications.activity.main
 
-import android.Manifest
 import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.core.app.NotificationCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,16 +18,17 @@ import com.github.tuvy22686.notifications.R
 import com.github.tuvy22686.notifications.adapter.MainListAdapter
 import com.github.tuvy22686.notifications.databinding.ActivityMainBinding
 import com.github.tuvy22686.notifications.listener.MainListItemClickListener
-import com.github.tuvy22686.notifications.push.LocalNotificationService
 import com.github.tuvy22686.notifications.viewmodel.main.MainViewModel
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity(), MainListItemClickListener {
 
     companion object {
-        val ACTION_LOCAL_PUSH: String? = MainActivity::class.java.canonicalName
-        const val REQUEST_CODE_LOCAL_PUSH: Int = 22686
-        const val REQUEST_CODE_PERMISSION: Int = 11343
+        const val CHANNEL_ID: String = "MAIN_ACTIVITY.CHANNEL_ID"
+
+        fun createIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
     }
 
     private val binding by lazy {
@@ -41,7 +42,6 @@ class MainActivity : AppCompatActivity(), MainListItemClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = mainViewModel
-        setupPermission()
         setupRecyclerView()
     }
 
@@ -52,36 +52,32 @@ class MainActivity : AppCompatActivity(), MainListItemClickListener {
         recyclerView.adapter = MainListAdapter(mainViewModel, this)
     }
 
-    private fun setupPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)) {
-                Toast.makeText(this, "設定から通知を許可してください", Toast.LENGTH_LONG).show()
-            } else {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECEIVE_SMS), REQUEST_CODE_PERMISSION)
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_CODE_PERMISSION) {
-            if (!(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                Toast.makeText(this, "パーミッションが許可されていません", Toast.LENGTH_LONG).show()
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
     override fun onToastItemClick() {
         Toast.makeText(applicationContext, "トーストメッセージ", Toast.LENGTH_LONG).show()
     }
 
     override fun onPushItemClick() {
-        println("MainActivity.onPushItemClick()")
-        val intent = Intent(this, LocalNotificationService::class.java)
-        intent.action = ACTION_LOCAL_PUSH
-        val sender = PendingIntent.getBroadcast(this, REQUEST_CODE_LOCAL_PUSH, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager.set(AlarmManager.RTC_WAKEUP, 0, sender)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val title = "Title"
+        val description = "Description"
+
+        if (Build.VERSION.SDK_INT >= 26 && notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
+            val channel = NotificationChannel(CHANNEL_ID, title, NotificationManager.IMPORTANCE_HIGH)
+            channel.description = description
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .apply {
+                setSmallIcon(R.drawable.ic_launcher_foreground)
+                setContentTitle(title)
+                setContentText(description)
+                setAutoCancel(true)
+                setWhen(System.currentTimeMillis())
+                setContentIntent(PendingIntent.getActivity(this@MainActivity, 0, createIntent(this@MainActivity), 0))
+            }
+            .build()
+        notificationManager.notify(AlarmManager.RTC_WAKEUP, notification)
     }
 
     override fun onInlineMessageItemClick() {
